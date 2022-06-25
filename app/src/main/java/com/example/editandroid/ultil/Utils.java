0d925b1695ca7992b64cdba84b8aae1a2da6a02a
khankhanh <94363870+khankhanh@users.noreply.github.com>
2022-06-25 10:47:22 +0700

    fixingagain

--- app/src/main/java/com/example/editandroid/ultil/Utils.java
+++ app/src/main/java/com/example/editandroid/ultil/Utils.java
@@ -5,5 +5,5 @@ import javax.net.ssl.SSLContext;
 import okhttp3.OkHttpClient;
 
 public class Utils {
-    public static final String BASE_URL="http://192.168.0.102/Banhang/";
+    public static final String BASE_URL="http://192.168.0.106/Banhang/";
 }
